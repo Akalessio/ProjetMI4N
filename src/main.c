9cd2472b74d4c5ee6074b557361508bf4183f1ac
Akalessio <waframesio@gmail.com>
2024-12-07 22:04:06 +0100

    added :  shell script sort csv  shell script make  shell script compilation  shell script tmp and graphs dir build  shell script execution

--- src/main.c
+++ src/main.c
@@ -5,6 +5,6 @@
 
 int main(int argc, char **argv) {
     printf("HELLO\n");
-    printf("%s\n%s\n%s\n", argv[1], argv[2], argv[3]);
+    printf("%s\n%s\n%s\n%s\n", argv[1], argv[2], argv[3], argv[4]);
     return 0;
 }
