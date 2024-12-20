04a866658c6cccf33f65cb72719ebd6e0bb3adce
Akalessio <138727150+Akalessio@users.noreply.github.com>
2024-12-20 16:18:04 +0100

    Update main.c

--- codeC/src/main.c
+++ codeC/src/main.c
@@ -6,17 +6,23 @@
 
 
 int main(int argc, char **argv) {
+    //create the station tree
     Station *sevastopol = stationFileReading();
 
+    //count the station
     int stationNumber = 0;
     stationCount(sevastopol, &stationNumber);
 
+    //print the number of station
     printf("\nthere are %d %s station\n\n", stationNumber, argv[2]);
 
+    //add the load to all the station
     sevastopol = userFileReading(sevastopol);
 
+    //create the output file
     createOutputFile(sevastopol, argv[2], argv[3], argv[4]);
 
+    //free the tree
     clearAVL(sevastopol);
 
     return 0;
